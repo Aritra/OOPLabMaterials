c193bfa2572ce8628771413920c6158507d81ec5
Aritra <kalpurush1601@gmail.com>
2024-06-26 12:20:38 +0530

    Add files via upload

---
+++ Lab_8/Global.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class Global {
+    public static HashMap<String, BITSStudent> myMap;
+
+    public static String[] getCGPAs(String q){
+        return q.split(",");
+    }
+
+    public static String printList(String primer, String[] a){
+        String r = "";
+        int n = a.length;
+        r = "list of "+primer+"s comprising of ";
+        for(int i=0;i<(n-1);i++){
+            r += a[i]+", ";
+        }
+        if(n==1){
+          r += "only";
+        }
+        else{
+          r += "and "+a[n-1];
+        }
+        return r;
+    }
+    
+}
---
+++ Lab_8/Major.java
@@ -0,0 +1,13 @@
+public enum Major {
+    Computer_Science,
+    Mechanical,
+    Electrical,
+    Electronics,
+    Civil,
+    Biotech,
+    Pharmacy,
+    Maths,
+    Physics,
+    Chemistry,
+    Economics;
+}
