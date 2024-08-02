957a001197dd84020f349112bc003d9da697af25
Aritra <kalpurush1601@gmail.com>
2024-08-02 10:52:35 +0530

    Delete Lab_9 directory

--- Lab_9/Main.java
+++
@@ -1,9 +0,0 @@
-class Main{
-   public static void main(String[] args) {
-      Buffer buffer = new Buffer(2);
-      Producer prod = new Producer(buffer);
-      Consumer cons = new Consumer(buffer);
-      prod.start();
-      cons.start();
-   }
-}
