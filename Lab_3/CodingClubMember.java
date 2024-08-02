08cc7128224f821e945475927407c34676fa0346
Aritra <kalpurush1601@gmail.com>
2024-08-02 10:53:10 +0530

    Delete Lab_3 directory

--- Lab_3/CodingClubMember.java
+++
@@ -1,7 +0,0 @@
-public interface CodingClubMember {
-    default String printClubName(){
-        return "The coding club called Crux";
-    }
-    public int memberRank();
-    public String[] languages();
-}
--- Lab_3/RoboticsClubMember.java
+++
@@ -1,7 +0,0 @@
-public interface RoboticsClubMember {
-    default String printRClubName(){
-        return "The robotics club called Asimov";
-    }
-    public int memberRankR();
-    public String[] robotTypes();
-}
