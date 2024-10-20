0b773ac2ba6d540f58b490c74e5e8af65abce11e
Aritra <kalpurush1601@gmail.com>
2024-10-20 18:57:10 +0530

    Add files via upload

---
+++ Lab2/BITSStudent.java
@@ -0,0 +1,10 @@
+public abstract class BITSStudent {
+    String name;
+    String ID;
+    String email;
+    String qualification;
+    int stipend;
+    public abstract void setQualification(String q); 
+    public abstract void setScholarship(String stipendClass);
+    public abstract void printDetails();
+}
